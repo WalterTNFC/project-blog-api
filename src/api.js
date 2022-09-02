0e247be78e4263305d4e1d3f1ae40c4f096b9898
WalterTNFC <walter.tad@hotmail.com>
2022-09-01 23:12:04 -0300

    feat: Created Endpoints

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const login = require('./controllers/login');
+const user = require('./controllers/user');
+const allUser = require('./controllers/allUsers');
+const userById = require('./controllers/userById');
+// const { verifyToken } = require('./validation/authorizationValidation');
 // ...
 
 const app = express();
@@ -7,6 +11,11 @@ const app = express();
 app.use(express.json());
 
 // ...
+app.use('/login', login);
+app.use('/user', user);
+// app.use(verifyToken);
+app.use('/user', allUser);
+app.use('/:id', userById);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
