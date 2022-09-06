a435d8f4b4855d3cf8f6790dc8398f81dc75651b
WalterTNFC <walter.tad@hotmail.com>
2022-09-05 21:15:39 -0300

    feat: update user model - associate hasMany

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -16,6 +16,12 @@ const User = (sequelize, DataTypes) => {
       timestamps: false,
       tableName: 'Users',
   });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,{
+        foreignKey: 'userId',
+        as: 'blogPosts',
+    });
+};
 
   return User;
 };
