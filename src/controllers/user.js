eb979d8bfac793511e5a39528bc2a8c8ce318455
WalterTNFC <walter.tad@hotmail.com>
2022-09-05 22:19:25 -0300

    fix: remove image

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,26 +6,22 @@ require('dotenv').config();
 
 const router = express.Router();
 router.post('/', async (req, res) => {
-  try {
-    const { displayName, email, password, image } = req.body; 
-    const user = await User.findOne({ where: { email } });
-    const { error: joiError } = userValidation.validate({ displayName, email, password, image });
-    console.log(`Aquiiiiiiiiiiiiiiiiiiiiiiiiii ${email}`);
-    if (joiError) {
-      return res.status(400).json({ message: joiError.details[0].message });
-    }
+  const { displayName, email, password, image } = req.body; 
+  const { error: joiError } = userValidation.validate({ displayName, email, password });
+  console.log(`Aquiiiiiiiiiiiiiiiiiiiiiiiiii ${email}`);
+  if (joiError) {
+    return res.status(400).json({ message: joiError.details[0].message });
+  }
+  
+  const user = await User.findOne({ where: { email } });
+  if (user) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
 
-    if (user) {
-      return res.status(409).json({ message: 'User already registered' });
-    }
+  await User.create({ displayName, email, password, image });
 
-    await User.create({ displayName, email, password, image });
-
-    const token = jwt.sign({ email }, process.env.JWT_SECRET);
-    return res.status(201).json({ token });
-  } catch (e) {
-      res.status(500).json({ message: 'Algo deu errado' });
-  } 
+  const token = jwt.sign({ displayName, email, image }, process.env.JWT_SECRET);
+  return res.status(201).json({ token });
 });
 
 module.exports = router;
